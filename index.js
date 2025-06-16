bc59ab83598b60c90eb8fcd3ac60824618a00b12
mikio <uaru0815@gmail.com>
2025-06-16 16:43:26 +0900

    add product api

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ db.exec(`
   )
 `);
 
+db.exec(`
+  CREATE TABLE IF NOT EXISTS products (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    price INTEGER,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  )
+`);
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -47,6 +57,48 @@ app.get("/api/orders", (req, res) => {
   const rows = db.prepare("SELECT * FROM orders ORDER BY id DESC").all();
   rows.forEach(r => r.items = JSON.parse(r.items));
   res.json(rows);
+});
+
+app.get("/api/products", (req, res) => {
+  const stmt = db.prepare("SELECT * FROM products ORDER BY id");
+  const products = stmt.all();
+  res.json(products);
+}
+);
+
+app.post("/api/products", (req, res) => {
+  const { name, price } = req.body;
+  if (!name || typeof price !== "number" || price < 0) {
+    return res.status(400).json({ error: "Invalid product data." });
+  }
+  const stmt = db.prepare(`
+    INSERT INTO products (name, price)
+    VALUES (?, ?)
+  `);
+  const info = stmt.run(name, price);
+  res.status(201).json({ id: info.lastInstertRowid });
+});
+
+app.put("/api/products/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, price } = req.body;
+
+  if (!name || typeof price !== "number" || price < 0) {
+    return res.json({ error: "Invalid product data." });
+  }
+
+  db.prepare(`
+    UPDATE products
+    SET name = ?, price = ?, updated_at = CURRENT_TIMESTAMP
+    WHERE id = ?
+  `).run(name, price, id);
+  res.json({ success: true });
+});
+
+app.delete("/api/products/:id", (req, res) => {
+  const { id } = req.params;
+  db.prepare(`DELETE FROM products WHERE id = ?`).run(id);
+  res.json({ success: true});
 });
 
 const PORT = 3000;
